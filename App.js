7a5fe15c5b66f5403468321e997d3b16f19fd6ab
JohnYacoub <johnboshra@gmail.com>
2021-04-17 11:30:27 -0700

    creating Homepage

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Tabs from './navigation/Tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {Home, Restaurant, OrderDelivery} from './screens';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName={'Home'}>
+        <Stack.Screen name="Home" component={Tabs} />
+        <Stack.Screen name="Restaurant" component={Restaurant} />
+        <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
